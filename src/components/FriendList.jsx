4cd1e60595bb7442d3d7e56a31586158a0041314
Monika Ghimire <ghimire.monika00@gmail.com>
2022-03-06 22:09:25 +0100

    added friend-list on user profile

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,19 +1,26 @@
 import React, { useContext } from 'react'
+import { ProfileContext } from '../contexts/ProfileContext'
 import { UserContext } from "../contexts/UserContext"
 
 const FriendList = () => {
     const [users, setUsers] = useContext(UserContext)
+    const [profile] = useContext(ProfileContext)
  
     const friends = users.sort(() => Math.random() - Math.random()).slice(0, 5)
    
     return (
-    <div>
-        FriendList
-        <ul>
-        {friends.map((friend,i )=> <li key={i}> <img src= { friend.picture.thumbnail }/>  {friend.name.first} {friend.name.last} </li>)}
-        </ul>
+
+        <>
+        {profile ? <div> 
+                        <p>FriendList</p>
+                        <ul>
+                        {friends.map((friend,i )=> <li key={i}> <img src= { friend.picture.thumbnail }/>  {friend.name.first} {friend.name.last} </li>)}
+                        </ul>
         
-        </div>
+                    </div>
+                    :
+                    <div />}
+        </>
     )
 }
 
