beaa79bc4140afea92a09735799a13416f4f4880
Melanie Ehanire <mehanire@outlook.com>
2022-03-13 12:56:21 +0100

    Slight edits and footer

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import {fashionArticles} from '../dataSources/fashionNews.js'
 import '../styles/Blog.scss'
 // import NewsApi from './NewsApiTest.jsx'
 
-console.log(fashionArticles);
+// console.log(fashionArticles);
 export default function Blog (){
 
     const [ posts, setPosts ] = useState(4)
@@ -16,17 +16,24 @@ export default function Blog (){
     // console.log("display post is " + displayPost);
 
     return(
-        <div>
-            <ul>
-            {postLimit.map(article => <li onClick={(e)=> setDisplayPost(e.target.innerText)}>{article.title}</li>)}</ul>
-            {/* <NewsApi/> */}
-           {currentArticle[0] != null &&  <div className='blogWrapper'>
-                                        <div className='blogPhoto' style={{backgroundImage:`url(${currentArticle[0].urlToImage})`}}></div>
-                                        <div className='blogText'>
-                                        <h3>{currentArticle[0].title}</h3>
-                                        <p>{currentArticle[0].content}</p>
-                                        </div>
+        <div className="blogWrapper">
+            <div className="flexWrapper">
+
+            {currentArticle[0] != null &&  <div className='blogStyles'>
+            
+                {/* <h3>{currentArticle[0].title}</h3> */}
+                <div className='blogPhoto' style={{backgroundImage:`url(${currentArticle[0].urlToImage})`}}></div>
+                                <div className='mText'>
+                                <h3>{currentArticle[0].title}</h3>
+                                <p>{currentArticle[0].content}</p>
+                             </div>
+                {/* <SaveComments/> */}
             </div>}
+            <ul> 
+            {postLimit.map(article => <li onClick={(e)=> setDisplayPost(e.target.innerText)}>{article.title}</li>)}
+            </ul>
+            
+            </div>
         </div>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
+import { FaTwitter } from 'react-icons/fa';
+import { FaFacebookSquare } from 'react-icons/fa';
+import { FaYoutubeSquare} from 'react-icons/fa';
+
+import '../styles/Footer.scss'
+
 
 export default function Footer (){
     return (
         <div className="footerWrapper">
-            Hi from Footer
+            <ul>
+                <li><FaTwitter color='lightblue'/></li>
+                <li className='fbIcon'><FaFacebookSquare color='blue'/></li>
+                <li><FaYoutubeSquare  color='red'/></li>
+                <li>instagram</li>
+            </ul>
+            <h4>HMM</h4>
         </div>
     )
 }
