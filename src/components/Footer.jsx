8f474cf41d14b7f5471fa6893ced179e5f953304
Melanie Ehanire <mehanire@outlook.com>
2022-03-13 14:13:23 +0100

    slight edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,30 @@
 import { FaTwitter } from 'react-icons/fa';
 import { FaFacebookSquare } from 'react-icons/fa';
 import { FaYoutubeSquare} from 'react-icons/fa';
+import { FaInstagramSquare } from 'react-icons/fa';
+import { FaCopyright } from 'react-icons/fa';
+
+
 
 import '../styles/Footer.scss'
 
 
 export default function Footer (){
+
+    const twitter = `https://twitter.com/`
+    const facebook = `https://facebook.com/`
+    const youtube = `https://www.youtube.com/`
+    const instagram = `https://www.instagram.com/?hl=de`
     return (
         <div className="footerWrapper">
+
             <ul>
-                <li><FaTwitter color='lightblue'/></li>
-                <li className='fbIcon'><FaFacebookSquare color='blue'/></li>
-                <li><FaYoutubeSquare  color='red'/></li>
-                <li>instagram</li>
+                <li><a href={twitter} target="_blank"><FaTwitter color='lightblue' className='icon' /> </a></li>
+                <li> <a href={facebook} target="_blank"><FaFacebookSquare color='blue'/></a></li>
+                <li>   <a href={youtube} target="_blank"><FaYoutubeSquare  color='red'/></a> </li>
+                <li> <a href={instagram} target="_blank">  < FaInstagramSquare color='lilac' /> </a></li>
             </ul>
-            <h4>HMM</h4>
+            <h4> <FaCopyright  color='aqua'/> 2022 Hmm</h4>
         </div>
     )
 }
