71f3321d49ec7ef108fed7368dce4d6f279c476d
hwbarrows@gmail.com <hwbarrows@gmail.com>
2022-03-15 09:31:07 +0100

    new styles from Monika for eye

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,51 +14,49 @@ export default function Login() {
   const [data, setData] = useState({ userName: "", password: "" });
   const [showSignUpForm, setShowSignUpForm] = useState(false);
   const [profile, setProfile] = useContext(ProfileContext);
-  const [loginAttempt, setLoginAttempt] = useState(false);
+  const [isUserNameNotValid, setIsUserNameNotValid] = useState(false);
+  const [isPasswordNotValid, setIsPasswordNotValid] = useState(false);
+
   // console.log(JSON.stringify(users.slice(0,20)));
 
-  // console.log(users);
-  //setUsers(...users, {test:"user"})
-  // console.log(users[0].login.password);
-  // console.log(users[0].login.username);
-
-  function checkUserName(e) {
+  function saveEnteredUserName(e) {
     setData({ ...data, userName: e.target.value });
   }
 
-  function checkPassword(e) {
+  function saveEnteredPassword(e) {
     setData({ ...data, password: e.target.value });
   }
 
   function submitLogin(e) {
     e.preventDefault();
+    if (data.userName == "" && data.password == ""){
+      return;
+    }
 
-    //Can use to confirm checkIfUserExist is truly empty
-    //if (array[index] != null) {
-    // The == and != operators consider null equal to only null or undefined
-    //}
+    const user = users.find((user) => user.login.username === data.userName);
 
-    const checkIfUserExist = users.filter(
-      (user) =>
-        user.login.username === data.userName &&
-        user.login.password === data.password
-    );
-    checkIfUserExist[0] != null && setProfile(...checkIfUserExist);
-    checkIfUserExist[0] != null && setIsLoggedIn(true);
-    checkIfUserExist[0] == null && setLoginAttempt(true);
-    // console.log(checkIfUserExist);
+    if (!user) {
+      setIsUserNameNotValid(true);
+      setIsPasswordNotValid(true);
+      return;
+    }
+    if (user.login.password != data.password) {
+      setIsUserNameNotValid(false);
+      setIsPasswordNotValid(true);
+      return;
+    }
+
+    setProfile(user);
+    setIsLoggedIn(true);
   }
 
-  console.log(users);
-
   return (
-    // <div className="loginWrapper">
     <>
       {(() => {
         if (isLoggedIn) {
           return <UserAccount />;
         }
-  
+
         return (
           <div className="loginWrapper">
             <div className="imgWrapper"></div>
@@ -66,7 +64,6 @@ export default function Login() {
               <SignUp
                 showSignUpForm={showSignUpForm}
                 setShowSignUpForm={setShowSignUpForm}
-              
               />
             )}
 
@@ -76,22 +73,27 @@ export default function Login() {
 
                 <input
                   className="loginInput"
-                  onChange={checkUserName}
+                  onChange={saveEnteredUserName}
                   type="text"
                   placeholder="Enter your username"
                 ></input>
 
-                <input
-                  className="loginInput"
-                  onChange={checkPassword}
+                <div className= "loginInput passwordStyleDiv">
+                  <input className="passwordStyleInput"
+                  
+                  
+                  onChange={saveEnteredPassword}
                   type={show ? "text" : "password"}
                   placeholder="Enter your password"
-                ></input>
-                <span className="eyeForLogin">
-                  <FaEye onMouseDown={() => setShow(!show)} />
-                </span>
+                  ></input>
 
-                {/* <button type='submit' onClick={submitLogin}>{ !isLoggedIn && !loginAttempt ? "login" : "Sign Up"}</button>     */}
+                  <span className="eyeStyling">
+                  {data.password && (
+                    <FaEye onMouseDown={() => setShow(!show)} />
+                  )}
+                  </span>
+                </div>
+                
 
                 <button type="submit" onClick={submitLogin}>
                   Log In
@@ -100,12 +102,119 @@ export default function Login() {
                 <a onClick={() => setShowSignUpForm(true)}>
                   New User? Sign up here{" "}
                 </a>
+                {isUserNameNotValid && <p>username not valid</p>}
+                {isPasswordNotValid && <p>password not valid</p>}
               </div>
+             
             )}
           </div>
         );
       })()}
     </>
-    // </div>
   );
 }
+
+
+
+// export default function Login() {
+//   const [show, setShow] = useState(false);
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const [users, setUsers] = useContext(UserContext);
+//   const [data, setData] = useState({ userName: "", password: "" });
+//   const [showSignUpForm, setShowSignUpForm] = useState(false);
+//   const [profile, setProfile] = useContext(ProfileContext);
+//   const [loginAttempt, setLoginAttempt] = useState(false);
+//   // console.log(JSON.stringify(users.slice(0,20)));
+
+//   // console.log(users);
+//   //setUsers(...users, {test:"user"})
+//   // console.log(users[0].login.password);
+//   // console.log(users[0].login.username);
+
+//   function checkUserName(e) {
+//     setData({ ...data, userName: e.target.value });
+//   }
+
+//   function checkPassword(e) {
+//     setData({ ...data, password: e.target.value });
+//   }
+
+//   function submitLogin(e) {
+//     e.preventDefault();
+
+//     //Can use to confirm checkIfUserExist is truly empty
+//     //if (array[index] != null) {
+//     // The == and != operators consider null equal to only null or undefined
+//     //}
+
+//     const checkIfUserExist = users.filter(
+//       (user) =>
+//         user.login.username === data.userName &&
+//         user.login.password === data.password
+//     );
+//     checkIfUserExist[0] != null && setProfile(...checkIfUserExist);
+//     checkIfUserExist[0] != null && setIsLoggedIn(true);
+//     checkIfUserExist[0] == null && setLoginAttempt(true);
+//     // console.log(checkIfUserExist);
+//   }
+
+//   console.log(users);
+
+//   return (
+//     // <div className="loginWrapper">
+//     <>
+//       {(() => {
+//         if (isLoggedIn) {
+//           return <UserAccount />;
+//         }
+  
+//         return (
+//           <div className="loginWrapper">
+//             <div className="imgWrapper"></div>
+//             {showSignUpForm && (
+//               <SignUp
+//                 showSignUpForm={showSignUpForm}
+//                 setShowSignUpForm={setShowSignUpForm}
+              
+//               />
+//             )}
+
+//             {!showSignUpForm && (
+//               <div className="loginForm">
+//                 <h3>Log In</h3>
+
+//                 <input
+//                   className="loginInput"
+//                   onChange={checkUserName}
+//                   type="text"
+//                   placeholder="Enter your username"
+//                 ></input>
+
+//                 <input
+//                   className="loginInput"
+//                   onChange={checkPassword}
+//                   type={show ? "text" : "password"}
+//                   placeholder="Enter your password"
+//                 ></input>
+//                 <span className="eyeForLogin">
+//                   <FaEye onMouseDown={() => setShow(!show)} />
+//                 </span>
+
+//                 {/* <button type='submit' onClick={submitLogin}>{ !isLoggedIn && !loginAttempt ? "login" : "Sign Up"}</button>     */}
+
+//                 <button type="submit" onClick={submitLogin}>
+//                   Log In
+//                 </button>
+
+//                 <a onClick={() => setShowSignUpForm(true)}>
+//                   New User? Sign up here{" "}
+//                 </a>
+//               </div>
+//             )}
+//           </div>
+//         );
+//       })()}
+//     </>
+//     // </div>
+//   );
+// }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -6,18 +6,24 @@ import "../styles/Login.scss";
 import "../styles/SignUp.scss";
 import astro from "../styles/astro.jpeg"
 
+
 const SignUp = ({ showSignUpForm, setShowSignUpForm }) => {
   const [users, setUsers] = useContext(UserContext);
   const [show, setShow] = useState(false)
   const [profile, setProfile] = useContext(ProfileContext);
   const [data, setData] = useState({});
-
+  const [isPasswordValid, setIsPasswordValid] = useState(true)
   const [isUserRegistered, setIsUserRegistered] = useState(false);
 
   function signUp(e) {
     // take in the new username and password in a new-user object, and create a new user profile.
     // console.log(users);
     e.preventDefault();
+    if(data.password.length < 8){
+      setIsPasswordValid(false);
+      return;
+    }
+
 
     const userObj = {
       login: {
@@ -32,9 +38,6 @@ const SignUp = ({ showSignUpForm, setShowSignUpForm }) => {
       email: data.email,
 
       picture: {
-        // medium: "http://www.formasdigitales.mx/images/client.png",
-        // large: "http://www.formasdigitales.mx/images/client.png",
-        // thumbnail: "http://www.formasdigitales.mx/images/client.png"
         medium: astro,
         large: astro,
         thumbnail: astro
@@ -46,11 +49,8 @@ const SignUp = ({ showSignUpForm, setShowSignUpForm }) => {
     if (Object.keys(data).length > 0) {
       setIsUserRegistered(true);
       setProfile(userObj);
-      // setShowSignUpForm(false)
     }
   }
-
-
 
   return (
     <>
@@ -88,24 +88,135 @@ const SignUp = ({ showSignUpForm, setShowSignUpForm }) => {
           placeholder="username"
         />
        
-        <input className="loginInput"
+       <div className="loginInput passwordStyleDiv">
+        <input 
+        className="passwordStyleInput"
           onChange={(e) => {
             setData({ ...data, password: e.target.value });
           }}
           type={show ? "text" : "password"}
           placeholder="password"
         />
-        <span className="eyeForSignUp">
-        <FaEye onClick={() => setShow(!show)} />
+        <span className="eyeStyling">
+        {data.password && <FaEye onClick={() => setShow(!show)} />}
         </span>
+        </div>
 
         <button className="signUpBtn" type="submit" onClick={signUp}>
           Sign up
         </button>
         <a onClick={() => setShowSignUpForm(false)}>Go back to Login </a>
+        {!isPasswordValid && <p>Password must contain 8 characters!</p>}
       </div>
     </>
   );
 };
 
 export default SignUp;
+
+
+
+
+// const SignUp = ({ showSignUpForm, setShowSignUpForm }) => {
+//   const [users, setUsers] = useContext(UserContext);
+//   const [show, setShow] = useState(false)
+//   const [profile, setProfile] = useContext(ProfileContext);
+//   const [data, setData] = useState({});
+
+//   const [isUserRegistered, setIsUserRegistered] = useState(false);
+
+//   function signUp(e) {
+//     // take in the new username and password in a new-user object, and create a new user profile.
+//     // console.log(users);
+//     e.preventDefault();
+
+//     const userObj = {
+//       login: {
+//         username: data.username,
+
+//         password: data.password,
+//       },
+//       name: {
+//         first: data.firstName,
+//         last: data.lastName,
+//       },
+//       email: data.email,
+
+//       picture: {
+//         // medium: "http://www.formasdigitales.mx/images/client.png",
+//         // large: "http://www.formasdigitales.mx/images/client.png",
+//         // thumbnail: "http://www.formasdigitales.mx/images/client.png"
+//         medium: astro,
+//         large: astro,
+//         thumbnail: astro
+     
+//       },
+//     };
+//     setUsers([...users, userObj]);
+
+//     if (Object.keys(data).length > 0) {
+//       setIsUserRegistered(true);
+//       setProfile(userObj);
+//       // setShowSignUpForm(false)
+//     }
+//   }
+
+
+
+//   return (
+//     <>
+//       <div className="loginForm">
+//         <h3>Sign Up</h3>
+//         <div className="nameInputs">
+//           <input className="loginNameInput"
+//             onChange={(e) => {
+//               setData({ ...data, firstName: e.target.value });
+//             }}
+//             type="text"
+//             placeholder="first name"
+//           />
+//           <input className="loginNameInput"
+//             onChange={(e) => {
+//               setData({ ...data, lastName: e.target.value });
+//             }}
+//             type="text"
+//             placeholder="last name"
+//           />
+//         </div>
+//         <input className="loginInput"
+//           onChange={(e) => {
+//             setData({ ...data, email: e.target.value });
+//           }}
+//           type="email"
+//           placeholder="email"
+//         />
+
+//         <input className="loginInput"
+//           onChange={(e) => {
+//             setData({ ...data, username: e.target.value });
+//           }}
+//           type="text"
+//           placeholder="username"
+//         />
+       
+//         <input className="loginInput"
+//           onChange={(e) => {
+//             setData({ ...data, password: e.target.value });
+//           }}
+//           type={show ? "text" : "password"}
+//           placeholder="password"
+//         />
+//         <span className="eyeForSignUp">
+//         <FaEye onClick={() => setShow(!show)} />
+//         </span>
+
+//         <button className="signUpBtn" type="submit" onClick={signUp}>
+//           Sign up
+//         </button>
+//         <a onClick={() => setShowSignUpForm(false)}>Go back to Login </a>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default SignUp;
