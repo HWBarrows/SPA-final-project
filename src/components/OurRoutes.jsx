de9e2330a184333b6271b09383b842573f9affd7
hwbarrows@gmail.com <hwbarrows@gmail.com>
2022-03-15 10:59:12 +0100

    additional styling after revert

--- src/components/OurRoutes.jsx
+++ src/components/OurRoutes.jsx
@@ -4,6 +4,7 @@ import Header from './Header.jsx'
 import MentalHealth from'./MentalHealth.jsx'
 import Music from './Music.jsx'
 import Beauty from './Beauty.jsx'
+import AboutUs from './AboutUs.jsx'
 
 export default function OurRoutes(){
 
@@ -17,6 +18,7 @@ export default function OurRoutes(){
             {/* <Route path="moreContent" element={<MoreCont/>}/> */}
             <Route path="mentalHealth" element={<MentalHealth/>}/>
             <Route path="beauty" element={<Beauty/>}/>
+            <Route path="aboutUS" element={<AboutUs/>}/>
         </Routes>
     )
 }
